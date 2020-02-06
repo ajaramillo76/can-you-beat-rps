9357d9a4e155a11b98f0b180228be24ee59a0153
Your Name Comes Here <archie808@hotmail.com>
2020-02-06 10:49:40 -0700

    Created landing page.

---
+++ app/src/main/java/edu/cnm/deepdive/canyoubeatrps/controller/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.canyoubeatrps.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.cnm.deepdive.canyoubeatrps.R;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
