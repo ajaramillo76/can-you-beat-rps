869cb4476d02f7b94ef943f9af9745bf3602570d
Your Name Comes Here <archie808@hotmail.com>
2020-02-10 08:55:06 -0700

    Edited User dao.

--- app/src/main/java/edu/cnm/deepdive/canyoubeatrps/model/dao/UserDao.java
+++ app/src/main/java/edu/cnm/deepdive/canyoubeatrps/model/dao/UserDao.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.canyoubeatrps.model.dao;
 
+import android.provider.ContactsContract.CommonDataKinds.Email;
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
@@ -7,6 +8,7 @@ import androidx.room.Insert;
 import androidx.room.Query;
 import androidx.room.Update;
 import edu.cnm.deepdive.canyoubeatrps.model.entity.User;
+import io.reactivex.Maybe;
 import io.reactivex.Single;
 import java.util.Collection;
 import java.util.List;
@@ -28,5 +30,8 @@ public interface UserDao {
 
   @Query("SELECT * FROM User ORDER BY user_id")
   LiveData<List<User>> select();
+
+  @Query("SELECT * FROM User WHERE email_id = :email")
+  Maybe<User> select(Email email);
 
 }
